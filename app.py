f6b47ac75ec700e2a4b4935e8401b10f54dba8c7
aleksandrkokov <kokov.alexander@gmail.com>
2023-05-15 14:42:33 +0200

    Mass calculator with bootstrap

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["POST"])
 def next_page():
-    height = int(request.form.get('height'))
-    weight = int(request.form.get('weight'))
-    bmi = weight/(height/100) ** 2
-    return render_template('next.html', bmi=bmi)
+    height = int(request.form.get("height"))
+    weight = int(request.form.get("weight"))
+    bmi = weight / (height / 100) ** 2
+    return render_template("next.html", bmi=bmi)
 
 
 if __name__ == "__main__":
